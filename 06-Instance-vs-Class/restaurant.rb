7cf132fdd2f001a475d1719e46905f8e9eb3c54c
Cédric M <me.cedric@gmail.com>
2014-01-20 15:30:29 +0100

    Solution exo 6 dossier OO

--- 06-Instance-vs-Class/restaurant.rb
+++ 06-Instance-vs-Class/restaurant.rb
@@ -1,3 +1,50 @@
 class Restaurant
-  # Your turn !
+
+	@global_rating = []
+
+	class << self
+		attr_accessor :global_rating
+
+		def avg_rating
+			Restaurant.global_rating.inject { |sum, element| sum + element } / Restaurant.global_rating.size 
+		end
+	end
+
+	def initialize(name, address, type_food, rating)
+		@name = name
+		@address = address
+		@type_food = type_food
+		@rating = []
+		@rating << rating
+		Restaurant.global_rating << rating
+	end
+
+	def restaurant_rating
+		@rating.inject { |sum, element| sum + element } / @rating.size 
+	end
+
+	def number_of_ratings
+		@rating.count
+	end
+
+	def rate(note)
+		@rating << note
+		Restaurant.global_rating << note
+	end
 end
+
+chez_momo = Restaurant.new("Chez Momo", "Rue des saligauds", "Charcuterie", 7)
+chez_alfred = Restaurant.new("Chez Alfred", "Rue des Pinèdes", "Poissonnerie", 8)
+
+chez_momo.rate(5)
+chez_momo.rate(3)
+chez_momo.rate(6)
+
+chez_alfred.rate(4)
+chez_alfred.rate(7)
+chez_alfred.rate(9)
+
+print chez_momo.restaurant_rating
+print chez_alfred.restaurant_rating
+
+puts Restaurant.global_rating
